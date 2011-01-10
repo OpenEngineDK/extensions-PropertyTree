d20e36fcee84bef0ddbc376bf8a250712a7bd500
Peter Kristensen <ptx@openengine.dk>
2011-01-10 14:38:58 +0000

    Updated yaml to 0.2.5

--- Utils/yaml/parserstate.h
+++ Utils/yaml/parserstate.h
@@ -6,20 +6,31 @@
 
 #include <string>
 #include <map>
+#include <stack>
+#include <cassert>
 
 namespace YAML
 {
 	struct Version {
+		bool isDefault;
 		int major, minor;
 	};
-
+	
 	struct ParserState
 	{
+		enum COLLECTION_TYPE { NONE, BLOCK_MAP, BLOCK_SEQ, FLOW_MAP, FLOW_SEQ, COMPACT_MAP };
+		
+		ParserState();
+
+		const std::string TranslateTagHandle(const std::string& handle) const;
+		COLLECTION_TYPE GetCurCollectionType() const { if(collectionStack.empty()) return NONE; return collectionStack.top(); }
+		
+		void PushCollectionType(COLLECTION_TYPE type) { collectionStack.push(type); }
+		void PopCollectionType(COLLECTION_TYPE type) { assert(type == GetCurCollectionType()); collectionStack.pop(); }
+	
 		Version version;
 		std::map <std::string, std::string> tags;
-
-		void Reset();
-		std::string TranslateTag(const std::string& handle) const;
+		std::stack <COLLECTION_TYPE> collectionStack;
 	};
 }
 
